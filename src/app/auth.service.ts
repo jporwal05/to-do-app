35db025b216241f2ea7dfcb44f16e7f9d146700f
Jayant Porwal <jayantporwal05@gmail.com>
2020-12-31 18:51:24 +0530

    Added auth service

---
+++ src/app/auth.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  BASE_URL = "http://localhost:8080/auth";
+  NAME_KEY = "name";
+  TOKEN_KEY = "token";
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  get firstName() {
+    return localStorage.getItem(this.NAME_KEY);
+  }
+
+  get isAuthenticated() {
+    return !!localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  register(user: any) {
+    delete user.confirmPassword;
+    this.http.post(this.BASE_URL + "/register", user).subscribe(response => {
+      let res: any = response;
+      if (!res.token) {
+        return;
+      }
+      localStorage.setItem(this.NAME_KEY, res.firstName);
+      localStorage.setItem(this.TOKEN_KEY, res.token);
+      this.router.navigate(["/"]);
+    }, (err) => {
+      throw err;
+    });
+  }
+
+  logout() {
+    localStorage.removeItem(this.NAME_KEY);
+    localStorage.removeItem(this.TOKEN_KEY);
+  }
+}
